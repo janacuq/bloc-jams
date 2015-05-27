ab1f8e3cf68ea900f390c57225b2f2d8cafbb853
janacuq <janacuq@gmail.com>
2015-05-27 15:26:15 +0100

    assignment22

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,21 +2,31 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints= function(points) {
 
-var revealPoint = function(index) {
-    for(var i = 0; i <= index; i++) {
+ points.forEach(revealPoint);
+    
+  function revealPoint(i) {
+    
                     points[i].style.opacity = 1;
                     points[i].style.transform = "scaleX(index) translateY(0)";
                     points[i].style.msTransform = "scaleX(index) translateY(0)";
                     points[i].style.WebkitTransform = "scaleX(index) translateY(0)";   
-    }
+    
  };
-                           
- revealPoint(2);
-
+                              
 };
+  
+
 
 window.onload = function() {
+    
+    if(window.innerHeight > 950) {
+        nimatePoints(pointsArray);
+    
+    }
+    
+    
     window.addEventListener("scroll", function(event) {
+        
         if(pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
