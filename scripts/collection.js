72f1355f64693d74cd2e69727e2dd39b703956f2
janacuq <janacuq@gmail.com>
2015-05-27 19:04:51 +0100

    ammendment 23

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,21 +1,22 @@
 var collectionItemTemplate = 
-    '<div class= "collection=album-container column fourth">'
-+'    <img src = "assets/images/alum_covers/01.png"/>'
-+'    <div class="collection-album-info caption">'
-+'        <p>'
-+'            <a class="album=name" href="/album.html"> The Colors </a>'
-+'            <br/>'
-+'            <a href="/album.html"> Pablo Picasso </a>'
-+'            <br/>'
-+'            X songs'
-+'            <br/>'
-+'        </p>'
-+'    </div>'
-+'    </div>';
+    '<div class="collection-album-container column fourth">'
+  + '  <img src="assets/images/album_covers/01.png"/>'
+  + '  <div class="collection-album-info caption">'
+  + '    <p>'
+  + '      <a class="album-name" href="/album.html"> The Colors </a>'
+  + '      <br/>'
+  + '      <a href="/album.html"> Pablo Picasso </a>'
+  + '      <br/>'
+  + '      X songs'
+  + '      <br/>'
+  + '    </p>'
+  + '  </div>'
+  + '</div>';
+
 
 window.onload = function() {
 
-    var collectionContainer = document.getElementByClassName('album-covers')[0].childNodes[1];
+    var collectionContainer = document.getElementsByClassName('album-covers')[0].childNodes[1];
     collectionContainer.innerHTML = '';
     
     for(var i = 0; i <12; i++) {
