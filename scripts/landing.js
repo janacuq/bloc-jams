8e16a2762f2fad766be4d7ea9066df53982f91c1
janacuq <janacuq@gmail.com>
2015-05-27 12:00:03 +0100

    assignment21

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,17 @@
  var animatePoints = function() {
 
                 var points = document.getElementsByClassName('point');
-               
-                var revealFirstPoint = function() {
-                    points[0].style.opacity = 1;
-                    points[0].style.transform = "scaleX(1) translateY(0)";
-                    points[0].style.msTransform = "scaleX(1) translateY(0)";
-                    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";   
-                };
+     
+     var revealPoint = function(x) {
+        for(var i = 0; i <= x; i++) {
+                    points[i].style.opacity = 1;
+                    points[i].style.transform = "scaleX(1) translateY(0)";
+                    points[i].style.msTransform = "scaleX(1) translateY(0)";
+                    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+      
+        }
+                
+     };
+revealPoint(2);
 
-                var revealSecondPoint = function() {
-                    points[1].style.opacity = 1;
-                    points[1].style.transform = "scaleX(1) translateY(0)";
-                    points[1].style.msTransform = "scaleX(1) translateY(0)";
-                    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";   
-                };
-
-                var revealThirdPoint = function() {
-                    points[2].style.opacity = 1;
-                    points[2].style.transform = "scaleX(1) translateY(0)";
-                    points[2].style.msTransform = "scaleX(1) translateY(0)";
-                    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";   
-                };
-               
-                revealFirstPoint();
-                revealSecondPoint();
-                revealThirdPoint();
-
-            };
-animatePoints();
+ };
