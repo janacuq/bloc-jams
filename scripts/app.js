b89377528c58ef6a29628fd198663fa1f7762715
janacuq <janacuq@gmail.com>
2015-06-05 17:20:10 +0100

    angular2

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+var myAppModule = angular.module('myapp',['ui-router']);
+
+myAppModule.config(['$stateProvider','$locationProvider', function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    
+    $stateProvider.state('album', {
+        url: '/album',
+        controller: 'Album.controller',
+        templateUrl: '/templates/album.html'
+    });
+
+    $stateProvider.state('landing', {
+        url: '/landing',
+        controller: 'Landing.controller',
+        templateUrl: '/templates/album.html'
+    });
+    
+    $stateProvider.state('collection', {
+        url: '/collection',
+        controller: 'Collection.controller',
+        templateUrl: '/templates/collection.html'
+    });
+}]);
