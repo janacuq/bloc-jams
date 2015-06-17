6c01721080e7a89278f35683ee88c74fa46d759a
janacuq <janacuq@gmail.com>
2015-06-17 21:12:29 +0100

    assignment2-angular

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
-var myAppModule = angular.module('myApp', []);
+var myAppModule = angular.module('myApp', ['ui.router']);
+
+myAppModule.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+    
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    
+    $stateProvider.state('landing', {
+        url: '/landing',
+        controller: 'Landing.controller',
+        temlplateUrl: '/templates/landing.html'
+    });
+    $stateProvider.state('album', {
+        url: '/album',
+        controller: 'Album.controller',
+        temlplateUrl: '/templates/album.html'
+    });
+    $stateProvider.state('collection', {
+        url: '/collection',
+        controller: 'Collection.controller',
+        temlplateUrl: '/templates/collection.html'
+    });
+    
+}]);
