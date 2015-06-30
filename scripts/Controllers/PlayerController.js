3e0fc2e7c87d984078cc91ab08dfbfeda690c58c
janacuq <janacuq@gmail.com>
2015-06-30 16:52:50 +0100

    adding volume bar

--- scripts/Controllers/PlayerController.js
+++ scripts/Controllers/PlayerController.js
@@ -1,5 +1,5 @@
 myAppModule.controller('PlayerController', ['$scope', 'MusicPlayer', function ($scope, MusicPlayer) {
-
+  window.skope = $scope;
   console.log('player controller initialized');
   $scope.volume = 0.9;
   $scope.progress = 0.2;
@@ -18,4 +18,9 @@ myAppModule.controller('PlayerController', ['$scope', 'MusicPlayer', function ($
       $scope.progress = newValue;
     });
   });
+    
+    $scope.$watch('volume', function(newValue, oldValue, scope) {
+        MusicPlayer.setVolume(newValue * 100);
+    });
+        
 }]);
